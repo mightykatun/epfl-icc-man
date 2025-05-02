af0fd701ae14ab47c75bbff7882309e978208fea
mightykatun <generalhellothere55555@gmail.com>
2025-05-02 16:01:18 +0200

    moved and updated fonction_parametre.py

--- Other scripts/fonction_parametre_an_b/fonctions_parametre.py
+++ Other scripts/fonction_parametre_an_b/fonctions_parametre.py
@@ -12,6 +12,10 @@ y_t2 = lambda t: -2*t
 t1_range = np.linspace(-2, 2, 1000)
 t2_range = np.linspace(-2, 2, 100)
 
+### plot
+
+fig = plt.figure()
+
 plt.plot(x_t1(t1_range), y_t1(t1_range), color='blue')
 # plt.plot(x_t2(t1_range), y_t2(t1_range), color='red')
 
@@ -20,4 +24,7 @@ for i in t2_range:
 	end_point = (start_point[0] + x_t2(i), start_point[1] + y_t2(i))
 	plt.plot([start_point[0], end_point[0]], [start_point[1], end_point[1]], color='gray', linestyle='-', linewidth=0.5)
 
+plt.grid()
 plt.show()
+
+fig.savefig('graph.png')
